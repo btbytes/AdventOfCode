3d259ce7710a03d1502971e3d51888ebe1b46cf4
Pradeep Gowda <pradeep@btbytes.com>
2020-12-01 11:19:01 -0500

    day 1 - part b - alternate solution 2 - python

--- 2020/day01-b-1.py
+++
@@ -1,9 +0,0 @@
-with open('01.txt') as f:
-	expenses = [l.strip() for l in f]
-expenses.sort()	
-
-for i in expenses:
-	for j in expenses:
-		for k in expenses:
-			if int(i)+int(j)+int(k) == 2020:
-				print(int(i) * int(j) * int(k))
---
+++ 2020/day01b2.py
@@ -0,0 +1,26 @@
+# Day 1 part b of the problem 
+# do not repeat over the numbers already picked
+# Output :
+# answer : 203481432
+# ops    : 3654046
+# time   : 1.32s
+
+
+import sys
+import time
+
+start = time.time()
+ops = 0
+
+with open('01.txt') as f:
+	expenses = [l.strip() for l in f]
+
+for ii, i in enumerate(expenses):
+	for jj, j in enumerate(expenses[ii:]):
+		for kk, k in enumerate(expenses[ii+jj:]):
+			ops += 3
+			if int(i)+int(j)+int(k) == 2020:
+				print(f'answer : {int(i) * int(j) * int(k)}')
+				print(f'ops    : {ops+1}')
+				print(f'time   : %.2fs' % (time.time() - start, ) )
+				sys.exit(0)
